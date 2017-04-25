142ad89ca295f2da1b703c9646f606943902af12
janety2373 <janetyu@newyorklife.com>
2017-04-25 11:48:54 -0400

    Corrected addition

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,7 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
+  notify { "Hello world! I am ${::fqdn}": }
   
   # example code for the classroom
   include examples::puppetize
