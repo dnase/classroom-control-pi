9d309338e815ebe597fe290777c954f93b67aad8
busytoday <busytoday@gmail.com>
2017-04-25 10:32:53 -0500

    lab 3.1 - update to hello world

--- manifests/site.pp
+++ manifests/site.pp
@@ -29,5 +29,5 @@ node default {
   # example code for the classroom
   include examples::puppetize
   
-  notify { "This is the default message from the production environment": }
+  notify { "Hello world! I am ${::fqdn}": }
 }
