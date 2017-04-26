359de8a181ef85505cdd0f901178d24244ae5260
vtx1000 <phamrichardv@gmail.com>
2017-04-26 09:13:58 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -20,9 +20,7 @@
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
-node 'rpham.puppetlabs.vm' {
-notify { "This will only be enforced on the Linux container.": }
-}
+
 node default {
 unless $environment in [ 'production', 'staging' ] {
 notify { "Warning: this is a development environment on ${::fqdn}": }
