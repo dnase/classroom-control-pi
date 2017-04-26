72bf069fabddc2497e9a05efab5f52f45c52e3f4
vtx1000 <phamrichardv@gmail.com>
2017-04-26 12:17:09 -0700

    Update init.pp

--- site/skeleton/examples/init.pp
+++ site/skeleton/examples/init.pp
@@ -1,1 +1,33 @@
 include skeleton
+class skeleton {
+
+  file { '/etc/skel':
+
+    ensure => directory,   # what value should go here?
+
+    owner  => 'root',
+
+    group  => 'root',
+
+    mode   => '0755',
+
+  }
+
+  
+
+  # add a resource to manage /etc/skel/.bashrc
+
+  file { '/etc/skel/.bashrc':
+
+    ensure => file,
+
+    owner => 'root',
+
+    group => 'root',
+
+    mode => '0644',
+
+    source => 'puppet:///modules/skeleton/bashrc',
+
+  }
+
