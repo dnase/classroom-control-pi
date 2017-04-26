30fc1cb53da0d25ebb926bf1eb6d2e2f213489d7
tinup-ku <tinup-ku@users.noreply.github.com>
2017-04-26 19:14:57 -0400

    Update redis.pp

--- site/profile/manifests/redis.pp
+++ site/profile/manifests/redis.pp
@@ -1,11 +1,6 @@
 class profile::redis {
-  # We'll require our epel class so that it is enforced first.
-  # Don't forget to update that class before enforcing your code
-  require profile::epel
-  
-  class { 'redis':
-    # what parameter should we pass to set maxmemory to 10mb?
-    
-  }
-  
+require profile::epel
+class { 'redis':
+maxmemory => '10mb',
+}
 }
