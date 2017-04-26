362e69c5d7daf696badc1897d53f77f2093a9c67
kurtpifer <kurt.i.pifer@gmail.com>
2017-04-26 10:13:56 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -28,11 +28,12 @@ node default {
   
   # example code for the classroom
   include examples::puppetize
-  
-  #notify { "This is the default message from the production environment": }
-  notify { "Caution: Happy Fun Ball may suddenly accelerate to dangerous speeds. ": }
+  unless $environment in [ 'production', 'staging' ] {
+    notify { "Warning: this is a development environment on ${::fqdn}": }
+  }
+    notify { "Caution: Happy Fun Ball may suddenly accelerate to dangerous speeds. ": }
 }
 
-node 'kurtpifer.puppetlabs.vm' {
+#node 'kurtpifer.puppetlabs.vm' {
   #notify {"Do not taunt happy fun ball.":}
-}
+#}
