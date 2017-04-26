5dd9ffe79e6fac56aa01896d29612948e11b42a6
shane-evans <shane.evans@daimler.com>
2017-04-26 12:02:23 -0700

    add correction
    fully qualified

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -7,7 +7,7 @@ class skeleton {
   }
   
   # add a resource to manage /etc/skel/.bashrc
-file {'etc/skel/.bashrc':
+file {'/etc/skel/.bashrc':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
